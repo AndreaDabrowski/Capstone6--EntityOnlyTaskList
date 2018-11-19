815daf659412438e4c5f90c6c3edc144d6ea980c
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-11-18 23:24:00 -0500

    everything works

---
+++ Capstone6NonIdentity/Models/CurrentUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone6NonIdentity.Models
+{
+    public class CurrentUser
+    {
+        public CurrentUser(bool logIn, int iD)
+        {
+            LogIn = logIn;
+            ID = iD;
+        }
+        public CurrentUser (bool logIn)
+        {
+            LogIn = logIn;
+            ID = 0;
+        }
+        public CurrentUser(int iD)
+        {
+            LogIn = false;
+            ID = iD;
+        }
+        public CurrentUser()
+        {
+            LogIn = false;
+            ID = 1;
+        }
+        public bool LogIn { get; set; }
+        public int ID { get; set; }
+    }
+}
